6aaedb7352e34632c67dee58c80d33466e53f677
Abigail Bayless <arbayls@users.noreply.github.com>
2022-06-10 21:13:07 -0700

    rename app to symphony

--- django_app/symphony_django/wsgi.py
+++ django_app/symphony_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for concert_django project.
+WSGI config for symphony_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'concert_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'symphony_django.settings')
 
 application = get_wsgi_application()
--- reactApp/src/App.js
+++ reactApp/src/App.js
@@ -9,7 +9,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a>
-          Welcome to Concert!
+          Welcome to Symphony!
         </a>
       </header>
     </div>

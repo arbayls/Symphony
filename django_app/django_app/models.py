a892896e9cd2a288b42f82429fbda5deda749afa
Abigail Bayless <arbayls@users.noreply.github.com>
2022-06-14 14:46:29 -0700

    update models

--- django_app/django_app/models.py
+++ django_app/django_app/models.py
@@ -2,19 +2,34 @@ from django.db import models
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    preferred_name = models.CharField(max_length=30)
     user_name = models.Charfield(max_length=30)
-    pronouns = models.TextChoices('She/Her They/Them He/Him')
+    pronouns = (
+        ('She', 'She/Her/Hers'),
+        ('They', 'They/Them/Theirs'),
+        ('He', 'He/Him/His'),
+        (preferred_name, 'No gender pronouns, please.')
+    )
+    pronouns = models.CharField(max_length=30, choices=pronouns)
 
 
 class Answer(models.Model):
-    name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
     life_sphere = models.CharField(max_length=30)
     user_answer = models.CharField(max_length=500)
 
 
 class Prompt(models.Model):
-    name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
     life_sphere = models.CharField(max_length=30)
-    text = models.CharField(max_length=500)
+    prompt_text = models.CharField(max_length=500)
+
+
+class Relationship(models.Model):
+    relationship_type = (
+        ('Romantic', 'Romantic Partner'),
+        ('Friend', 'Friendship'),
+        ('Roommate', 'Roommates'),
+        ('Other', 'Something Else')
+    )
+    type = models.CharField(max_length=30, choices=relationship_type)

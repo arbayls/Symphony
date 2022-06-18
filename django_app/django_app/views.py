41d14be70bb21f2761f5ec2c2cb58647daf90a0a
Abigail Bayless <arbayls@users.noreply.github.com>
2022-06-18 12:07:13 -0700

    REST methods added

--- django_app/django_app/views.py
+++ django_app/django_app/views.py
@@ -10,35 +10,55 @@ from rest_framework.decorators import api_view
 from django.core.signals import request_finished
 
 
-
 def index(request):
     return HttpResponse("Hello, world. You're at the symphony index.")
 
 
+# many
 @api_view(['GET', 'POST', 'DELETE'])
 def user_list(request):
     if request.method == 'GET':
         users = User.objects.all()
 
-        title = request.GET.get('title', None)
-        if title is not None:
-            users = User.filter(title__icontains=title)
+        user_name = request.GET.get('user_name', None)
+        if user_name is not None:
+            users = User.filter(user_name__icontains=user_name)
 
         users_serializer = UserSerializer(users, many=True)
         return JsonResponse(users_serializer.data, safe=False)
         # 'safe=False' for objects serialization
 
-    # @api_view(['GET', 'PUT', 'DELETE'])
-    # def tutorial_detail(request, pk):
-    #     # find tutorial by pk (id)
-    #     try:
-    #         tutorial = Tutorial.objects.get(pk=pk)
-    #     except Tutorial.DoesNotExist:
-    #         return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
-    #
-    #     # GET / PUT / DELETE tutorial
-    #
-    #
-    # @api_view(['GET'])
-    # def tutorial_list_published(request):
-    #     # GET all published tutorials
+    elif request.method == 'POST':
+        user_data = JSONParser().parse(request)
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return JsonResponse(user_serializer.data,
+                                status=status.HTTP_201_CREATED)
+        return JsonResponse(user_serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user_name = request.GET.get('user_name', None)
+        if user_name is not None:
+            filtered_users = User.filter(user_name__icontains=user_name)
+            filtered_users.delete()
+            return JsonResponse({'message': 'Users were deleted successfully.'},
+                                status=status.HTTP_204_NO_CONTENT)
+
+
+
+# one
+@api_view(['GET', 'DELETE'])
+def user(request, pk):
+    if request.method == 'GET':
+        user = User.objects.get(pk=pk)
+
+        user_serializer = UserSerializer(user)
+        return JsonResponse(user_serializer.data)
+
+    elif request.method == 'DELETE':
+        user = User.objects.get(pk=pk)
+        user.delete()
+        return JsonResponse({'message': 'User was deleted successfully.'},
+                            status=status.HTTP_204_NO_CONTENT)
